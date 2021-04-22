1d4321f5c1ce71d857607e80d83ba658aa4f95e0
Batman <montevechiol@gmail.com>
2021-04-22 00:32:49 -0300

    Fucking row change

--- src/views/navigator/result-lists/result-list/result/NavigatorResultView.tsx
+++ src/views/navigator/result-lists/result-list/result/NavigatorResultView.tsx
@@ -39,21 +39,21 @@ export function NavigatorResultView(props: NavigatorResultViewProps): JSX.Elemen
     }
 
     return (
-        <div className="d-flex flex-column justify-content-center align-items-center nitro-navigator-result" onClick={ () => navigatorContext.onTryVisitRoom(result) }>
+        <div className="d-flex flex-column justify-content-center align-items-center nitro-navigator-result small" onClick={ () => navigatorContext.onTryVisitRoom(result) }>
             <div className="d-flex justify-content-between w-100 px-2 py-1">
                 <div className="d-flex justify-content-center flex-grow-1 overflow-hidden">
                     <div className={ "d-flex justify-content-center align-items-center badge text-center " + getUserCounterColor() }>
-                        <i className="fas fa-user mr-1 small"></i> { result.userCount }
+                        <i className="fas fa-user mr-1"></i> { result.userCount }
                     </div>
                     <div className="d-flex flex-column justify-content-center align-items-start flex-grow-1 px-2 overflow-hidden">
                         <span className="d-block text-truncate" style={ { maxWidth: '95%' } }>{ result.roomName }</span>
                     </div>
                 </div>
                 <div className="d-flex flex-row-reverse align-items-center">
-                    <i className="fas fa-info-circle small" onClick={ openInfo }></i>
-                    { result.habboGroupId > 0 && <i className="fas fa-users mr-2 small"></i> }
+                    <i className="fas fa-info-circle" onClick={ openInfo }></i>
+                    { result.habboGroupId > 0 && <i className="fas fa-users mr-2"></i> }
                     { result.doorMode !== RoomDataParser.OPEN_STATE && 
-                        <i className={ 'mr-2 fas small ' + classNames( {'fa-lock': result.doorMode === RoomDataParser.DOORBELL_STATE, 'fa-key': result.doorMode === RoomDataParser.PASSWORD_STATE })}></i>
+                        <i className={ 'mr-2 fas ' + classNames( {'fa-lock': result.doorMode === RoomDataParser.DOORBELL_STATE, 'fa-key': result.doorMode === RoomDataParser.PASSWORD_STATE })}></i>
                     }
                 </div>
             </div>
