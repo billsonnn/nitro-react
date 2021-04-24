0cc1c1d278c7f4eb03903737bfa417b02178602e
Bill <billsonnn@live.com>
2021-04-23 21:05:37 -0400

    Update chat input

--- src/views/room/widgets/chat-input/ChatInputView.tsx
+++ src/views/room/widgets/chat-input/ChatInputView.tsx
@@ -5,7 +5,7 @@ import { ChatInputViewProps } from './ChatInputView.types';
 export const ChatInputView: FC<ChatInputViewProps> = props =>
 {
     return (
-        <div className="nitro-chat-form fixed-bottom mb-4 d-flex justify-content-center">
+        <div className="nitro-chat-input fixed-bottom mb-4 d-flex justify-content-center">
             <div className="nitro-chat-form-input">
                 <input type="text" className="form-control" placeholder={ LocalizeText('widgets.chatinput.default') } />
             </div>
