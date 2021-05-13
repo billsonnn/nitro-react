2d1d2f2e3f5e64ee49698fa4fa51fb55be26c348
MyNameIsBatman <montevechiol@gmail.com>
2021-05-12 21:49:00 -0300

    Change header text back

--- src/layout/card/header/NitroCardHeaderView.tsx
+++ src/layout/card/header/NitroCardHeaderView.tsx
@@ -8,7 +8,7 @@ export const NitroCardHeaderView: FC<NitroCardHeaderViewProps> = props =>
     return (
         <div className="drag-handler d-flex align-items-center bg-primary px-2 py-1 nitro-card-header">
             <div className="d-flex flex-grow-1 justify-content-center align-items-center">
-                <div className="h5 m-0 text-white nitro-text-shadow">{ headerText }</div>
+                <div className="h4 m-0 text-white text-shadow">{ headerText }</div>
             </div>
             <div className="cursor-pointer" onClick={ onCloseClick }>
                 <i className="fas fa-times"></i>
