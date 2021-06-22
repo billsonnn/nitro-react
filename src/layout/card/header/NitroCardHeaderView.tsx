c2f2aeed1e012e0f3d382eff8f3f23097fe991cf
Bill <billsonnn@live.com>
2021-06-22 05:52:58 -0400

    Update close button

--- src/layout/card/header/NitroCardHeaderView.tsx
+++ src/layout/card/header/NitroCardHeaderView.tsx
@@ -10,12 +10,10 @@ export const NitroCardHeaderView: FC<NitroCardHeaderViewProps> = props =>
     return (
         <div className={ `drag-handler container-fluid bg-${ theme }` }>
             <div className="row nitro-card-header">
-                <div className="col-10 offset-1 d-flex justify-content-center align-items-center">
+                <div className="d-flex justify-content-center align-items-center w-100 position-relative">
                     <div className="h4 m-0 text-white text-shadow">{ headerText }</div>
-                </div>
-                <div className="nitro-card-close-parent">
-                    <div className="nitro-card-close cursor-pointer" onMouseDown={ event => event.stopPropagation() } onClick={ onCloseClick }>
-                        <i className="fas fa-times"></i>
+                    <div className="position-absolute nitro-card-close" onMouseDown={ event => event.stopPropagation() } onClick={ onCloseClick }>
+                        <i className="fas fa-times" />
                     </div>
                 </div>
             </div>
