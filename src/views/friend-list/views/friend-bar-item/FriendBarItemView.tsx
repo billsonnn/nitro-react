7780eb65925bd2308f1171e694d323396e2bb86e
Layne <laynebalsters@gmail.com>
2021-07-03 20:45:26 -0400

    friend bar changes

--- src/views/friend-list/views/friend-bar-item/FriendBarItemView.tsx
+++ src/views/friend-list/views/friend-bar-item/FriendBarItemView.tsx
@@ -1,18 +1,66 @@
-import { FC } from 'react';
+import { FollowFriendComposer, MouseEventType, Nitro } from 'nitro-renderer';
+import { FC, useEffect, useRef, useState } from 'react';
+import { LocalizeText } from '../../../../utils/LocalizeText';
+import { AvatarImageView } from '../../../shared/avatar-image/AvatarImageView';
 import { FriendBarItemViewProps } from './FriendBarItemView.types';
 
 export const FriendBarItemView: FC<FriendBarItemViewProps> = props =>
 {
     const { friend = null } = props;
+    const [isVisible, setVisible] = useState(false);
+
+    const toggleVisible = () => setVisible(prevCheck => !prevCheck);
+
+    const elementRef = useRef<HTMLDivElement>();
+
+    useEffect(() =>
+    {
+        function onClick(event: MouseEvent): void
+        {
+            const element = elementRef.current;
+
+            if((event.target !== element) && !element.contains((event.target as Node)))
+            {
+                setVisible(false);
+            }
+        }
+
+        document.addEventListener(MouseEventType.MOUSE_CLICK, onClick);
+
+        return () =>
+        {
+            document.removeEventListener(MouseEventType.MOUSE_CLICK, onClick);
+        }
+    }, [ elementRef, setVisible ]);
+    
+
+    const followFriend = () =>
+    {
+
+        Nitro.instance.communication.connection.send(new FollowFriendComposer(friend.id));
+    }
 
     if(!friend)
     {
         return (
-            <div>offline</div>
+            <div ref={ elementRef } className="btn btn-primary friend-bar-item friend-bar-search">
+                <div className="friend-bar-item-head position-absolute"/>
+                <div className="text-truncate">{ LocalizeText("friend.bar.find.title") }</div>
+            </div>
         );
     }
 
     return (
-        <div>{ friend.name }</div>
+        <div ref={ elementRef } className={"btn btn-success friend-bar-item " + (isVisible ? "friend-bar-item-active" : "")} onClick={ event => toggleVisible()}>
+            <div className="friend-bar-item-head position-absolute">
+                <AvatarImageView headOnly={true} figure={friend.figure} direction={2} />
+            </div>
+            <div className="text-truncate">{friend.name}</div>
+            {isVisible && <div className="d-flex justify-content-between">
+                <i className="icon icon-fb-chat cursor-pointer" />
+                {friend.followingAllowed && <i onClick={ event => followFriend() } className="icon icon-fb-visit cursor-pointer" />}
+                <i className="icon icon-fb-profile cursor-pointer" />
+            </div>}
+        </div>
     );
 }
--- src/views/friend-list/views/friend-bar/FriendBarView.tsx
+++ src/views/friend-list/views/friend-bar/FriendBarView.tsx
@@ -24,12 +24,16 @@ export const FriendBarView: FC<FriendBarViewProps> = props =>
     }, [ indexOffset, friends ]);
 
     return (
-        <div className="d-flex">
-            <button type="button" className="btn btn-sm btn-primary" disabled={ !canDecreaseIndex } onClick={ event => setIndexOffset(indexOffset - 1) }>back</button>
+        <div className="d-flex friend-bar align-items-center">
+            <button type="button" className="btn btn-sm btn-black align-self-center friend-bar-button" disabled={!canDecreaseIndex} onClick={event => setIndexOffset(indexOffset - 1)}>
+                <i className="fas fa-chevron-left" />
+            </button>
             <FriendBarItemView friend={ (friends[ indexOffset ] || null) } />
             <FriendBarItemView friend={ (friends[ indexOffset + 1 ] || null) } />
             <FriendBarItemView friend={ (friends[ indexOffset + 2 ] || null) } />
-            <button type="button" className="btn btn-sm btn-primary" disabled={ !canIncreaseIndex } onClick={ event => setIndexOffset(indexOffset + 1) }>forward</button>
+            <button type="button" className="btn btn-sm btn-black align-self-center friend-bar-button" disabled={!canIncreaseIndex} onClick={event => setIndexOffset(indexOffset + 1)}>
+                <i className="fas fa-chevron-right" />
+            </button>
         </div>
     );
 }
