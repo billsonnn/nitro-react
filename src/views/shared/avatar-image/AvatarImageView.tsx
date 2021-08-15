56961a448c3c080d123393f138b22d67032fae1b
Bill <billsonnn@live.com>
2021-08-15 02:24:10 -0400

    Update AvatarImageView

--- src/views/shared/avatar-image/AvatarImageView.tsx
+++ src/views/shared/avatar-image/AvatarImageView.tsx
@@ -1,21 +1,23 @@
 import { AvatarScaleType, AvatarSetType } from '@nitrots/nitro-renderer';
-import { FC, useEffect, useState } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
 import { GetAvatarRenderManager } from '../../../api';
 import { AvatarImageViewProps } from './AvatarImageView.types';
 
 export const AvatarImageView: FC<AvatarImageViewProps> = props =>
 {
     const { figure = '', gender = 'M', headOnly = false, direction = 0, scale = 1 } = props;
-
     const [ avatarUrl, setAvatarUrl ] = useState<string>(null);
     const [ randomValue, setRandomValue ] = useState(-1);
+    const isDisposed = useRef(false);
 
     useEffect(() =>
     {
-        if(randomValue) {}
-
         const avatarImage = GetAvatarRenderManager().createAvatarImage(figure, AvatarScaleType.LARGE, gender, {
-            resetFigure: figure => setRandomValue(Math.random()),
+            resetFigure: figure => {
+                if(isDisposed.current) return;
+
+                setRandomValue(Math.random());
+            },
             dispose: () => {},
             disposed: false
         }, null);
@@ -34,6 +36,16 @@ export const AvatarImageView: FC<AvatarImageViewProps> = props =>
 
         avatarImage.dispose();
     }, [ figure, gender, direction, headOnly, randomValue ]);
+
+    useEffect(() =>
+    {
+        isDisposed.current = false;
+
+        return () =>
+        {
+            isDisposed.current = true;
+        }
+    }, []);
 
     const url = `url('${ avatarUrl }')`;
         
