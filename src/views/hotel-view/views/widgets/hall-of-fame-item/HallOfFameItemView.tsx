a2ed795cd2ba6985673526e8bf55308d392b1421
MyNameIsBatman <montevechiol@gmail.com>
2021-09-03 16:47:37 -0300

    Update HallOfFame

--- src/views/hotel-view/views/widgets/hall-of-fame-item/HallOfFameItemView.tsx
+++ src/views/hotel-view/views/widgets/hall-of-fame-item/HallOfFameItemView.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { LocalizeText } from '../../../../../api';
 import { AvatarImageView } from '../../../../shared/avatar-image/AvatarImageView';
+import { UserProfileIconView } from '../../../../shared/user-profile-icon/UserProfileIconView';
 import { HallOfFameItemViewProps } from './HallOfFameItemView.types';
 
 export const HallOfFameItemView: FC<HallOfFameItemViewProps> = props =>
@@ -11,7 +12,7 @@ export const HallOfFameItemView: FC<HallOfFameItemViewProps> = props =>
         <div className="hof-user-container cursor-pointer">
             <div className="hof-tooltip">
                 <div className="hof-tooltip-content">
-                    <div className="fw-bold">{ level }. { data.userName }</div>
+                    <div className="fw-bold">{ level }. { data.userName } <UserProfileIconView userId={ data.userId } /></div>
                     <div className="muted fst-italic small text-center">{ LocalizeText('landing.view.competition.hof.points', [ 'points' ], [ data.currentScore.toString() ])}</div>
                 </div>
             </div>
