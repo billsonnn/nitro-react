f69b40bbfe58d4e2f4c78dd6b44e431f7ddb5959
Bill <billsonnn@live.com>
2021-09-29 22:30:01 -0400

    Update BadgeImageView

--- src/views/shared/badge-image/BadgeImageView.types.ts
+++ src/views/shared/badge-image/BadgeImageView.types.ts
@@ -1,7 +1,11 @@
-export interface BadgeImageViewProps
+import { NitroLayoutBaseProps } from '../../../layout/base';
+
+export interface BadgeImageViewProps extends NitroLayoutBaseProps
 {
     badgeCode: string;
     isGroup?: boolean;
     showInfo?: boolean;
     customTitle?: string;
+    isGrayscale?: boolean;
+    scale?: number;
 }
