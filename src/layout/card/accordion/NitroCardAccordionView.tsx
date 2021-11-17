a5fcea1cd7afbfd2051fdd3845fe52542ddd4770
Bill <billsonnn@live.com>
2021-11-17 16:38:36 -0500

    Layout accordion updates

--- src/layout/card/accordion/NitroCardAccordionView.tsx
+++ src/layout/card/accordion/NitroCardAccordionView.tsx
@@ -1,13 +1,32 @@
-import { FC } from 'react';
+import { FC, useCallback, useMemo, useState } from 'react';
+import { NitroLayoutFlexColumn } from '../..';
+import { NitroCardAccordionContextProvider } from './NitroCardAccordionContext';
 import { NitroCardAccordionViewProps } from './NitroCardAccordionView.types';
 
 export const NitroCardAccordionView: FC<NitroCardAccordionViewProps> = props =>
 {
-    const { className = '' } = props;
+    const { className = '', children = null, ...rest } = props;
+    const [ closers, setClosers ] = useState<Function[]>([]);
+
+    const getClassName = useMemo(() =>
+    {
+        let newClassName = 'nitro-card-accordion text-black';
+
+        if(className && className.length) newClassName += ` ${ className }`;
+
+        return newClassName;
+    }, [ className ]);
+
+    const closeAll = useCallback(() =>
+    {
+        for(const closer of closers) closer();
+    }, [ closers ]);
 
     return (
-        <div className={ 'nitro-card-accordion text-black ' + className }>
-            { props.children }
-        </div>
+        <NitroCardAccordionContextProvider value={ { closers, setClosers, closeAll } }>
+            <NitroLayoutFlexColumn className={ getClassName } { ...rest }>
+                { children }
+            </NitroLayoutFlexColumn>
+        </NitroCardAccordionContextProvider>
     );
 }
