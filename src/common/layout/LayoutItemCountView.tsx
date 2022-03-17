124e50b75e978f51eaa3c72aa04f533790e11034
Layne <laynebalsters@gmail.com>
2022-03-17 06:29:06 -0400

    changed item-count font sizes

--- src/common/layout/LayoutItemCountView.tsx
+++ src/common/layout/LayoutItemCountView.tsx
@@ -12,7 +12,7 @@ export const LayoutItemCountView: FC<LayoutItemCountViewProps> = props =>
 
     const getClassNames = useMemo(() =>
     {
-        const newClassNames: string[] = [ 'badge', 'border', 'bg-danger', 'px-1', 'rounded-circle', 'nitro-item-count' ];
+        const newClassNames: string[] = [ 'badge', 'border', 'border-black', 'bg-danger', 'px-1', 'nitro-item-count' ];
 
         if(classNames.length) newClassNames.push(...classNames);
 
