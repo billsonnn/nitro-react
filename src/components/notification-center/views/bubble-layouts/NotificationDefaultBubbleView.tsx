6e4c5e26b7f39718b0404a608b05c1b42c4aa22e
Bill <billsonnn@live.com>
2022-03-21 02:18:34 -0400

    Fix bubble click url

--- src/components/notification-center/views/bubble-layouts/NotificationDefaultBubbleView.tsx
+++ src/components/notification-center/views/bubble-layouts/NotificationDefaultBubbleView.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { NotificationBubbleItem } from '../../../../api';
+import { NotificationBubbleItem, NotificationUtilities } from '../../../../api';
 import { Flex, LayoutNotificationBubbleView, LayoutNotificationBubbleViewProps, Text } from '../../../../common';
 
 export interface NotificationDefaultBubbleViewProps extends LayoutNotificationBubbleViewProps
@@ -13,8 +13,10 @@ export const NotificationDefaultBubbleView: FC<NotificationDefaultBubbleViewProp
     
     const htmlText = item.message.replace(/\r\n|\r|\n/g, '<br />');
 
+    NotificationUtilities.openUrl(item.linkUrl);
+
     return (
-        <LayoutNotificationBubbleView close={ close } gap={ 2 } alignItems="center" { ...rest }>
+        <LayoutNotificationBubbleView close={ close } gap={ 2 } alignItems="center" onClick={ event => (item.linkUrl && item.linkUrl.length && NotificationUtilities.openUrl(item.linkUrl)) } { ...rest }>
             <Flex center className="bubble-image-container">
                 { (item.iconUrl && item.iconUrl.length) &&
                     <img className="no-select" src={ item.iconUrl } alt="" /> }
