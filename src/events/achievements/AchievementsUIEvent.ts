ad42e1113cbbd6fd9b46633cb4873fa2c9aac095
Bill <billsonnn@live.com>
2022-03-23 03:05:57 -0400

    Remove events

--- src/events/achievements/AchievementsUIEvent.ts
+++
@@ -1,8 +0,0 @@
-import { NitroEvent } from '@nitrots/nitro-renderer';
-
-export class AchievementsUIEvent extends NitroEvent
-{
-    public static SHOW_ACHIEVEMENTS: string = 'AE_SHOW_ACHIEVEMENTS';
-    public static HIDE_ACHIEVEMENTS: string = 'AE_HIDE_ACHIEVEMENTS';
-    public static TOGGLE_ACHIEVEMENTS: string = 'AE_TOGGLE_ACHIEVEMENTS';
-}
--- src/events/achievements/index.ts
+++ src/events/achievements/index.ts
@@ -1,2 +1,1 @@
-export * from './AchievementsUIEvent';
 export * from './AchievementsUIUnseenCountEvent';
