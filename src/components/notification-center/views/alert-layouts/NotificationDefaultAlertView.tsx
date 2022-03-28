03d176b53a7c4eecd305a7e1fba88bc25f1d08be
Layne <laynebalsters@gmail.com>
2022-03-28 03:13:44 +0000

    different notifications n sizes

--- src/components/notification-center/views/alert-layouts/NotificationDefaultAlertView.tsx
+++ src/components/notification-center/views/alert-layouts/NotificationDefaultAlertView.tsx
@@ -18,32 +18,37 @@ export const NotificationDefaultAlertView: FC<NotificationDefaultAlertViewProps>
         NotificationUtilities.openUrl(item.clickUrl);
         
         close();
-    }, [ item, close ]);
+    }, [item, close]);
     
-
-    const isAction = (item.clickUrl && item.clickUrl.startsWith('event:'));
-
-    const hasFrank = item.alertType === NotificationAlertType.DEFAULT || item.alertType === NotificationAlertType.MODERATION;
+    const hasFrank = item.alertType === NotificationAlertType.DEFAULT;
 
     return (
-        <LayoutNotificationAlertView title={title} close={close} {...rest}>
-            <Flex fullHeight overflow="auto" gap={2}>
-                {hasFrank && !item.imageUrl && <Base className="notification-frank flex-shrink-0" /> }
-                {item.imageUrl && !imageFailed && <img src={item.imageUrl} alt={ item.title } onError={() => { setImageFailed(true) } } /> }
+        <LayoutNotificationAlertView title={title} close={close} {...rest} type={ hasFrank ? NotificationAlertType.DEFAULT : item.alertType }>
+            <Flex fullHeight overflow="auto" gap={ hasFrank || (item.imageUrl && !imageFailed) ? 2 : 0 }>
+            {hasFrank && !item.imageUrl && <Base className="notification-frank flex-shrink-0" /> }
+            {item.imageUrl && !imageFailed && <img src={item.imageUrl} alt={item.title} onError={() => { setImageFailed(true) }} className="align-self-baseline" />}
+            <Base classNames={['notification-text overflow-y-auto d-flex flex-column w-100', (item.clickUrl && !hasFrank) ? 'justify-content-center' : '']}>
                 { (item.messages.length > 0) && item.messages.map((message, index) =>
                 {
                     const htmlText = message.replace(/\r\n|\r|\n/g, '<br />');
 
-                    return <Base grow fullHeight overflow="auto" key={ index } dangerouslySetInnerHTML={ { __html: htmlText } } />;
+                    return <Base key={ index } dangerouslySetInnerHTML={ { __html: htmlText } } />;
                 })}
+                    {item.clickUrl && (item.clickUrl.length > 0) && (item.imageUrl && !imageFailed) && <>
+                        <hr className="my-2 w-100" />
+                        <Button onClick={visitUrl} className="align-self-center px-3">{LocalizeText(item.clickUrlText)}</Button>
+                    </>}
+            </Base>
             </Flex>
-            <hr className="my-2"/>
-            <Column alignItems="center" center gap={ 1 }>
-                { !isAction && !item.clickUrl &&
-                    <Button onClick={ close }>{ LocalizeText('generic.close') }</Button> }
-                { item.clickUrl && (item.clickUrl.length > 0) &&
-                    <Button onClick={ visitUrl }>{ LocalizeText(item.clickUrlText) }</Button> }
-            </Column>
+            { (!item.imageUrl || (item.imageUrl && imageFailed)) && <>
+                <Column alignItems="center" center gap={0}>
+                    <hr className="my-2 w-100" />
+                    { !item.clickUrl &&
+                        <Button onClick={close}>{LocalizeText('generic.close')}</Button>}
+                    { item.clickUrl && (item.clickUrl.length > 0) && <Button onClick={visitUrl}>{LocalizeText(item.clickUrlText)}</Button> }
+                </Column>
+            </> }
         </LayoutNotificationAlertView>
     );
+
 }
