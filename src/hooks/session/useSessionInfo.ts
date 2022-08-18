981e0d65f6ed4618348ff1260f708ca1a6d61853
Bill <billsonnn@live.com>
2022-08-18 01:45:04 -0400

    Reset the sizes if the screen size changes

--- src/hooks/session/useSessionInfo.ts
+++ src/hooks/session/useSessionInfo.ts
@@ -1,8 +1,9 @@
 import { FigureUpdateEvent, RoomUnitChatStyleComposer, UserInfoDataParser, UserInfoEvent, UserSettingsEvent } from '@nitrots/nitro-renderer';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useBetween } from 'use-between';
-import { GetSessionDataManager, SendMessageComposer } from '../../api';
+import { GetLocalStorage, GetSessionDataManager, SendMessageComposer } from '../../api';
 import { useMessageEvent } from '../events';
+import { useLocalStorage } from '../useLocalStorage';
 
 const useSessionInfoState = () =>
 {
@@ -11,6 +12,7 @@ const useSessionInfoState = () =>
     const [ chatStyleId, setChatStyleId ] = useState<number>(0);
     const [ userRespectRemaining, setUserRespectRemaining ] = useState<number>(0);
     const [ petRespectRemaining, setPetRespectRemaining ] = useState<number>(0);
+    const [ screenSize, setScreenSize ] = useLocalStorage('nitro.screensize', { width: window.innerWidth, height: window.innerHeight });
 
     const updateChatStyleId = (styleId: number) =>
     {
@@ -56,6 +58,34 @@ const useSessionInfoState = () =>
 
         setChatStyleId(parser.chatType);
     });
+
+    useEffect(() =>
+    {
+        const currentScreenSize = <{ width: number, height: number }>GetLocalStorage('nitro.screensize');
+
+        if(currentScreenSize && ((currentScreenSize.width !== window.innerWidth) || (currentScreenSize.height !== window.innerHeight)))
+        {
+            let i = window.localStorage.length;
+
+            while(i > 0)
+            {
+                const key = window.localStorage.key(i);
+    
+                if(key && key.startsWith('nitro.window')) window.localStorage.removeItem(key);
+    
+                i--;
+            }
+        }
+
+        const onResize = (event: UIEvent) => setScreenSize({ width: window.innerWidth, height: window.innerHeight });
+
+        window.addEventListener('resize', onResize);
+
+        return () =>
+        {
+            window.removeEventListener('resize', onResize);
+        }
+    }, [ setScreenSize ]);
 
     return { userInfo, userFigure, chatStyleId, userRespectRemaining, petRespectRemaining, respectUser, respectPet, updateChatStyleId };
 }
