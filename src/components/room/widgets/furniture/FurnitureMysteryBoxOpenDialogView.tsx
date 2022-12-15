082bae8f31a95cf5903fb6417b971d1f540a1fe5
dank074 <torresefrain10@gmail.com>
2022-12-14 18:00:59 -0600

    convert mode to enum

--- src/components/room/widgets/furniture/FurnitureMysteryBoxOpenDialogView.tsx
+++ src/components/room/widgets/furniture/FurnitureMysteryBoxOpenDialogView.tsx
@@ -15,27 +15,33 @@ type PrizeData = {
     classId:number;
 }
 
+enum ViewMode {
+    HIDDEN,
+    WAITING,
+    PRIZE
+}
+
 export const FurnitureMysteryBoxOpenDialogView: FC<FurnitureMysteryBoxOpenDialogViewProps> = props =>
 {
     const { ownerId = -1 } = props;
-    const [ mode, setMode ] = useState<'hidden' | 'waiting' | 'prize'>('hidden');
+    const [ mode, setMode ] = useState<ViewMode>(ViewMode.HIDDEN);
     const [ prizeData, setPrizeData ] = useState<PrizeData>(undefined);
 
     const close = () =>
     {
-        if(mode === 'waiting') SendMessageComposer(new MysteryBoxWaitingCanceledMessageComposer(ownerId));
-        setMode('hidden');
+        if(mode === ViewMode.WAITING) SendMessageComposer(new MysteryBoxWaitingCanceledMessageComposer(ownerId));
+        setMode(ViewMode.HIDDEN);
         setPrizeData(undefined);
     }
 
     useMessageEvent<ShowMysteryBoxWaitMessageEvent>(ShowMysteryBoxWaitMessageEvent, event =>
     {
-        setMode('waiting');
+        setMode(ViewMode.WAITING);
     });
 
     useMessageEvent<CancelMysteryBoxWaitMessageEvent>(CancelMysteryBoxWaitMessageEvent, event =>
     {
-        setMode('hidden');
+        setMode(ViewMode.HIDDEN);
         setPrizeData(undefined);
     });
 
@@ -43,25 +49,25 @@ export const FurnitureMysteryBoxOpenDialogView: FC<FurnitureMysteryBoxOpenDialog
     {
         const parser = event.getParser();
         setPrizeData({ contentType: parser.contentType, classId: parser.classId });
-        setMode('prize');
+        setMode(ViewMode.PRIZE);
     });
 
     const isOwner = GetSessionDataManager().userId === ownerId;
 
-    if(mode === 'hidden') return null;
+    if(mode === ViewMode.HIDDEN) return null;
 
     return (
         <NitroCardView className="nitro-mysterybox-dialog" theme="primary-slim">
-            <NitroCardHeaderView headerText={ mode === 'waiting' ? LocalizeText(`mysterybox.dialog.${ isOwner ? 'owner' : 'other' }.title`) : LocalizeText('mysterybox.reward.title') } onCloseClick={ close } />
+            <NitroCardHeaderView headerText={ mode === ViewMode.WAITING ? LocalizeText(`mysterybox.dialog.${ isOwner ? 'owner' : 'other' }.title`) : LocalizeText('mysterybox.reward.title') } onCloseClick={ close } />
             <NitroCardContentView>
-                { mode === 'waiting' && <>
+                { mode === ViewMode.WAITING && <>
                     <Text variant="primary"> { LocalizeText(`mysterybox.dialog.${ isOwner ? 'owner' : 'other' }.subtitle`) } </Text>
                     <Text> { LocalizeText(`mysterybox.dialog.${ isOwner ? 'owner' : 'other' }.description`) } </Text>
                     <Text> { LocalizeText(`mysterybox.dialog.${ isOwner ? 'owner' : 'other' }.waiting`) }</Text>
                     <Button variant="danger" onClick={ close } className="mt-auto"> { LocalizeText(`mysterybox.dialog.${ isOwner ? 'owner' : 'other' }.cancel`) } </Button>
                 </>
                 }
-                { mode === 'prize' && prizeData && <>
+                { mode === ViewMode.PRIZE && prizeData && <>
                     <Text variant="black"> { LocalizeText('mysterybox.reward.text') } </Text>
                     <Flex className="prize-container justify-content-center mx-auto">
                         <LayoutPrizeProductImageView classId={ prizeData.classId } productType={ prizeData.contentType }/>
