a0144818879f5b2e284524345cb10bd2479d854d
dank074 <torresefrain10@gmail.com>
2022-12-22 15:39:57 -0600

    catch possible permission error from localstorage

--- src/api/utils/GetLocalStorage.ts
+++ src/api/utils/GetLocalStorage.ts
@@ -1,1 +1,11 @@
-export const GetLocalStorage = <T>(key: string) => JSON.parse(window.localStorage.getItem(key)) as T ?? null;
+export const GetLocalStorage = <T>(key: string) =>
+{
+    try
+    {
+        JSON.parse(window.localStorage.getItem(key)) as T ?? null
+    }
+    catch(e)
+    {
+        return null;
+    }
+}
