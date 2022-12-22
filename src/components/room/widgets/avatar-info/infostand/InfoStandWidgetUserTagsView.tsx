f7909c32ac69c896b448feeb1789c4e6f78500ca
oobjectt <objectholos@gmail.com>
2022-12-23 00:38:26 +0100

    Feature - User tags

---
+++ src/components/room/widgets/avatar-info/infostand/InfoStandWidgetUserTagsView.tsx
@@ -0,0 +1,31 @@
+import { NavigatorSearchComposer } from '@nitrots/nitro-renderer';
+import { FC } from 'react';
+import { CreateLinkEvent, SendMessageComposer } from '../../../../../api';
+import { Flex, Text } from '../../../../../common';
+
+interface InfoStandWidgetUserTagsViewProps
+{
+    tags: string[];
+}
+
+const processAction = (tag: string) =>
+{
+    CreateLinkEvent(`navigator/search/${ tag }`);
+    SendMessageComposer(new NavigatorSearchComposer('hotel_view', `tag:${ tag }`));
+}
+
+export const InfoStandWidgetUserTagsView: FC<InfoStandWidgetUserTagsViewProps> = props =>
+{
+    const { tags = null } = props;
+
+    if(!tags || !tags.length) return null;
+
+    return (
+        <>
+            <hr className="m-0" />
+            <Flex className="flex-tags">
+                { tags && (tags.length > 0) && tags.map((tag, index) => <Text key={ index } variant="white" className="text-tags" onClick={ event => processAction(tag) }>{ tag }</Text>) }
+            </Flex>
+        </>
+    );
+}
